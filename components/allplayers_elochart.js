dcbc60fdce1be05d8adb097bcf6ef2bed0d25319
Jorge Cordoba <jorge.cs@gmail.com>
2022-01-15 13:21:53 +0800

    Modified to use react chart js 2

--- components/allplayers_elochart.js
+++ components/allplayers_elochart.js
@@ -1,25 +1,57 @@
+import 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
 
-import { LineChart,CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, ResponsiveContainer } from "recharts";   
+export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: true,
+        text: 'Elo evolution',
+      },
+    },
+  };
 
 export function AllPlayersEloChart(props) {
-    let lines = []    
+    let data = {
+        labes: Object.keys(props.nicks),
+        datasets: []
+    }
     
     for (var entry in props.nicks) {
         var randomColor = Math.floor(Math.random()*16777215).toString(16);
-        lines.push(<Line type="monotone" connectNulls={true} dataKey={entry} stroke={`#${randomColor}`} />)
-    }
+        console.log(entry)
+        data.datasets.push( {
+            label: props.nicks[entry].nick,
+            borderColor: props.nicks[entry].color ? props.nicks[entry].color : `#${randomColor}`,
+            cubicInterpolationMode: 'monotone',       
+            lineTension: 0.1,     
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: props.nicks[entry].color ? props.nicks[entry].color : `#${randomColor}`,
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            spanGaps: true,
+            data: props.data,
+            hidden: !props.nicks[entry].main,
+            parsing: {
+                yAxisKey: entry,
+                xAxisKey: 'match_date'
+            }
+        })
+    }    
 
-    return (
-        <ResponsiveContainer width='100%' height={350}>
-        <LineChart height={350} data={props.data}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis domain={['dataMin - 50', 'dataMax + 50']}/>
-            <Tooltip />
-            <Legend />            
-            {lines}            
-        </LineChart>
-    </ResponsiveContainer>
+    return (        
+        <Line options={options} data={data}>      
+        </Line>    
     );
 };
