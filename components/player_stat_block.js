549c3ebfd6cce061ee403d9467d822648bd71805
Jorge Córdoba <jorge.cs@gmail.com>
2022-07-29 16:03:40 +0200

    Implemented loop to read players

--- components/player_stat_block.js
+++ components/player_stat_block.js
@@ -4,19 +4,17 @@ import {Row, Col, Card} from "react-bootstrap"
 
 export function PlayerStatBlock(props) {
     props.accountStats.sort((a,b) => (b.current_tier*100 + b.current_raking) - (a.current_tier * 100 + a.current_raking))
-    console.log(props.accountStats)
     return (
-       <Fragment>
-        <Card className='shadow' style={{ padding: '12px'}}>            
-            <Row>
-                <Col><PlayerCard player={props.accountStats[0]}></PlayerCard></Col>
-                <Col><PlayerCard player={props.accountStats[1]}></PlayerCard></Col>
-                <Col><PlayerCard player={props.accountStats[2]}></PlayerCard></Col>
-                <Col><PlayerCard player={props.accountStats[3]}></PlayerCard></Col>
-                <Col><PlayerCard player={props.accountStats[4]}></PlayerCard></Col>
-                <Col><PlayerCard player={props.accountStats[5]}></PlayerCard></Col>                
+    <Fragment>
+        <Card className='shadow' style={{ padding: '12px', paddingTop: '2px'}}>                 
+            <Row>{
+                props.accountStats.map( p => {
+                    return (                        
+                        <Col><PlayerCard player={p}></PlayerCard></Col>                                              
+                    )
+                })}
             </Row>
-        </Card>            
-       </Fragment> 
+        </Card>  
+    </Fragment>
     );
 };
