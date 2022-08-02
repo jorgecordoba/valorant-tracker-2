a4b2eb7d6adda18073a0fa81c30bf14a6061d8f7
Jorge Córdoba <jorge.cs@gmail.com>
2022-08-02 16:50:45 +0200

    Added GA

---
+++ components/player_details.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+import { Col, Row } from "react-bootstrap"
+
+const PlayerDetails = (props) => {
+    return (
+        <Fragment>
+            <Row>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+        </Fragment>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.scss'
+import { GoogleAnalytics, usePageViews, event } from "nextjs-google-analytics";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export function reportWebVitals({ id, name, label, value }) {
+  event(name, {
+    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
+
+function MyApp({ Component, pageProps }) {  
+  usePageViews();
+  return (
+    <> 
+    <GoogleAnalytics />   
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
